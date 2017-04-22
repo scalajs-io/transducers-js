3130d4f00c033e5a61adc5f50db02fefc57ad8ca
Lawrence Daniels <lawrence.daniels@gmail.com>
2017-04-22 01:36:49 -0700

    Upgraded to Scalajs.io v0.4.0-pre5

--- src/main/scala/io/scalajs/npm/transducers/package.scala
+++ src/main/scala/io/scalajs/npm/transducers/package.scala
@@ -1,7 +1,7 @@
 package io.scalajs.npm
 
 import scala.scalajs.js
-import scala.scalajs.js.annotation.JSName
+import scala.scalajs.js.annotation.JSGlobal
 
 /**
   * transducers package object
@@ -13,29 +13,29 @@ package object transducers {
     * Transducers entry-point for browser-based applications
     */
   @js.native
-  @JSName("transducers")
+  @JSGlobal("transducers")
   object transducers extends TransducersJs
 
   @js.native
-  @JSName("com.cognitect.transducers.isArray")
+  @JSGlobal("com.cognitect.transducers.isArray")
   object isArray extends js.Object {
     def apply(value: js.Any): Boolean = js.native
   }
 
   @js.native
-  @JSName("com.cognitect.transducers.isObject")
+  @JSGlobal("com.cognitect.transducers.isObject")
   object isObject extends js.Object {
     def apply(value: js.Any): Boolean = js.native
   }
 
   @js.native
-  @JSName("com.cognitect.transducers.isString")
+  @JSGlobal("com.cognitect.transducers.isString")
   object isString extends js.Object {
     def apply(value: js.Any): Boolean = js.native
   }
 
   @js.native
-  @JSName("com.cognitect.transducers.wrap")
+  @JSGlobal("com.cognitect.transducers.wrap")
   object wrap extends js.Object {
     def apply[T <: js.Any](value: T): js.Function0[T] = js.native
   }
